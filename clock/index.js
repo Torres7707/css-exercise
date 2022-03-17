d34619f23b5a7da1c25ac6b8dfd79e5858bc52f5
wangdong <wangd-aa@glodon.com>
2022-03-17 10:28:25 +0800

    update clock

--- clock/index.js
+++ clock/index.js
@@ -5,3 +5,5 @@ const s = d.getSeconds();
 time.style.setProperty('--ds', s);
 time.style.setProperty('--dm', m + s / 60);
 time.style.setProperty('--dh', h + m / 60 + s / 3600);
+
+// https://mp.weixin.qq.com/s/Mz8wXlV3LzlKvVi3Blr6_w
